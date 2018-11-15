b6cbfbe92ba78d4b72f83e4ebf8e2dd918561c55
wanglelecc <wanglelecc@gmail.com>
2018-11-15 13:34:27 +0800

    upate user model path

--- database/migrations/2018_02_25_170926_seed_roles_and_permissions_data.php
+++ database/migrations/2018_02_25_170926_seed_roles_and_permissions_data.php
@@ -20,7 +20,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
-use App\User;
+use App\Models\User;
 
 class SeedRolesAndPermissionsData extends Migration
 {
